65fd185275c2791a503d4a111a70ef2548af1996
Xiaocong Lau <12184795@qq.com>
2018-10-04 14:05:40 +0800

    Control selector from data config

--- js/config/data.js
+++ js/config/data.js
@@ -1,22 +1,55 @@
+// type -  0 : normal ， 1：text,  2: attr
 export default {
-  globalKey:'photo',
-  name:'摄影',
-  list:[
+  globalKey: 'photo',
+  name: '摄影',
+  list: [
     {
-      key:'foto',
-      name:'FOTO',
+      key: 'foto',
+      name: 'FOTO',
       url: 'https://www.fotobeginner.com/category/skills/',
-      nav:{
-        selector:'',
-        url:'',
-        titleSelector:'',
-        desSelector:'',
-        imgSelector:'',
-        contentUrlSelector:'',
+      nav: {
+        selector: [
+          {
+            type: 0,
+            value: '.archive-list',
+          },
+          {
+            type: 0,
+            value: '.recent-excerpts',
+          }],
+        titleSelector: [
+          {
+            type: 1,
+            value: 'h4',
+          }],
+        urlSelector: [
+          {
+            type: 0,
+            value: '.sec_img a',
+          },
+          {
+            type: 2,
+            value: 'href',
+          }],
+        desSelector: [
+          {
+            type: 1,
+            value: '.sec_desc p',
+          }],
+        imgSelector: [
+          {
+            type: 0,
+            value: '.sec_img a img',
+          },
+          {
+            type: 2,
+            value: 'src',
+          }],
+        contentUrlSelector: '',
       },
-      content:{
-        selector:'',
-      }
-    }
-  ]
-}
+      content: {
+        selector: '',
+      },
+    },
+  ],
+};
--- js/net/readingNet.js
+++ js/net/readingNet.js
@@ -17,24 +17,65 @@ export function getTargetReadingSide(globalKey, data, dispatch) {
   then((text) => {
     //let $ = cio.load(text);
     const $ = cheerio.load(text);
-    let titleList = $('.archive-list').find('.recent-excerpts');
+    const { selector, titleSelector, urlSelector, desSelector, imgSelector } = data.nav;
+    let titleList = getValueFromSelectors($, selector);
     let titles = [];
     for (let i = 0; i < titleList.length; i++) {
       let obj = {};
-      obj.title = titleList.eq(i).find('h4').text();
-      obj.des = titleList.eq(i).find('.sec_desc p').text();
-      obj.image = titleList.eq(i).find('.sec_img a img').attr('src');
-      obj.link = titleList.eq(i).find('.sec_img a').attr('href');
+      obj.title = getValueFromItemSelectors( titleList.eq(i), titleSelector);
+      obj.link = getValueFromItemSelectors( titleList.eq(i), urlSelector);
+      obj.des = getValueFromItemSelectors( titleList.eq(i), desSelector);
+      obj.image = getValueFromItemSelectors( titleList.eq(i), imgSelector);
       titles.push(obj);
     }
     dispatch(updateReadingTitles(globalKey, data.key, titles));
-    for (let i = 0; i<titles.length; i++){
+    for (let i = 0; i < titles.length; i++) {
       getTargetContent(i, globalKey, data.key, titles[i], dispatch);
     }
   }).
   catch((error) => {
     console.error(error);
   });
+}
+
+export function getValueFromSelectors($, selectors) {
+  let ret;
+  for (let i = 0; i < selectors.length; i++) {
+    if (i === 0) {
+      if (selectors[i].type === 0){
+        //normal
+        ret = $(selectors[i].value);
+      } else if (selectors[i].type === 1){
+        ret = $(selectors[i].value).text();
+      } else if (selectors[i].type === 2){
+
+      }
+    } else {
+      if (selectors[i].type === 0){
+        //normal
+        ret = ret.find(selectors[i].value);
+      } else if (selectors[i].type === 1){
+        ret = ret.find(selectors[i].value).text();
+      } else if (selectors[i].type === 2){
+
+      }
+    }
+  }
+  return ret;
+}
+
+export function getValueFromItemSelectors(item, selectors) {
+  for (let i = 0; i < selectors.length; i++){
+    if (selectors[i].type === 0) {
+      //normal
+      item = item.find(selectors[i].value);
+    } else if (selectors[i].type === 1) {
+      item = item.find(selectors[i].value).text();
+    } else if (selectors[i].type === 2) {
+      item = item.attr(selectors[i].value);
+    }
+  }
+  return item;
 }
 
 export function getTargetContent(index, globalKey, key, obj, dispatch) {
